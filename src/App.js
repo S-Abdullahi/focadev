42a270234846f7fc56816ac342cc524e00ad3cce
Abdullahi Salawu <salabdullah33@gmail.coom>
2022-09-08 13:17:02 +0100

    add post

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Feed from './component/feed'
 import LeftPanel from './component/leftpanel'
 import Menubar from './component/menubar'
 import Side from "./component/sidebar"
 import Addpost from "./component/addpost"
 import { Stack, Box } from '@mui/system'
+import { ThemeProvider } from '@emotion/react'
+import { createTheme } from '@mui/material'
 
 const App = () =>{
+    const [mode, setMode] = useState('dark')
+
+    const darkTheme = createTheme({
+        palette:{
+            mode: mode
+        }
+    })
     return (
         <>
-        <Box>
-            <Menubar/>
-            <Stack direction="row" spacing={2} justifyContent="space-between" >
-                <LeftPanel/>
-                <Feed/>
-                <Side/>
-            </Stack>
-            <Addpost position/>
-        </Box>
-            
-           
+                <Box>
+                    <Menubar/>
+                        <Stack direction="row" spacing={2} justifyContent="space-between" >
+                            <LeftPanel/>
+                            <Feed/>
+                            <Side/>
+                        </Stack>
+                    <Addpost position/>
+                </Box>
         </>
     )
 }
--- src/component/addpost.jsx
+++ src/component/addpost.jsx
@@ -1,15 +1,78 @@
 import React from 'react';
-import {Tooltip, Fab} from '@mui/material/';
+import {Tooltip, Fab, Modal, Box, Typography, Avatar, TextField, Stack, ButtonGroup, Button} from '@mui/material/';
 import AddIcon from '@mui/icons-material/Add';
+import { useState } from 'react';
+import { styled} from '@mui/material/styles';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import ImageIcon from '@mui/icons-material/Image';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import DateRangeIcon from '@mui/icons-material/DateRange';
+
+const StyledBox = styled(Box)(({theme})=>({
+    backgroundColor : "white",
+    padding: '20px',
+    borderRadius:2,
+    height: 280,
+    width: 400,
+}))
+
+const UserBox = styled(Box)(({theme})=>({
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+    marginBottom: '10px'
+}))
+
+const StyledModal = styled(Modal)(({theme})=>({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}))
 
 const Addpost = () => {
+    const [open, setOpen] = useState(false)
     return (
         <>
-            <Tooltip title="Delete" sx={{position:'fixed', left:30, bottom:20}}>
+            <Tooltip title="Delete" sx={{position:'fixed', bottom:20, left:{xs:'calc(50%)', md: 30}}} onClick={(e)=>setOpen(true)}>
             <Fab color="primary" aria-label="add">
                 <AddIcon />
             </Fab>
             </Tooltip>
+
+            <StyledModal
+                open={open}
+                onClose={(e)=>setOpen(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                >
+                <StyledBox>
+                   <Typography variant="h6" color="gray" textAlign="center">Create Post</Typography>
+                   <UserBox>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                    <Typography variant='span' fontWeight={500}>Salawu Abdullahi</Typography>
+                </UserBox>
+                <TextField
+                    id="standard-multiline-static"
+                    sx={{width:'100%'}}
+                    label="what is on your mind"
+                    multiline
+                    rows={3}
+                    placeholder="What is on your mind?"
+                    variant="standard"/>
+
+                <Stack direction='row' gap={1} mt={1} mb={2}>
+                    <EmojiEmotionsIcon color='primary'/>
+                    <ImageIcon color='secondary'/>
+                    <VideocamIcon color='success'/>
+                    <PersonAddIcon color='error'/>
+                </Stack>
+                <ButtonGroup variant="contained" aria-label="outlined primary button group" sx={{width:'100%'}}>
+                    <Button sx={{width:'100%'}}>Post</Button>
+                    <Button ><DateRangeIcon/></Button>
+                </ButtonGroup>
+                </StyledBox>
+            </StyledModal>
         </>
     )
 }
